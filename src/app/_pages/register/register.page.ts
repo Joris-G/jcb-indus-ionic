7f237737f32d9979cb686b213b07fef5f332fd1c
MelineBr <meline42@hotmail.fr>
2023-11-09 13:37:17 +0100

    init register page

--- src/app/_pages/register/register.page.ts
+++ src/app/_pages/register/register.page.ts
@@ -1,14 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
+
 export class RegisterPage implements OnInit {
+  public registerForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      nom: ['', Validators.required],
+      prenom: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      role: ['basic'], // Basic est la valeur par défaut
+      service: [''],
+      projets: [''],
+      motDePasse: ['', Validators.required]
+    });
+  }
 
+  onRegisterClick() {
+console.log(this.registerForm.value);
+  }
+  
   ngOnInit() {
   }
 
