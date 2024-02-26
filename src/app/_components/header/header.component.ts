a9f8f7329ade3d5edc9c629419d713b852759b81
Joris-G <66126635+Joris-G@users.noreply.github.com>
2024-02-26 21:11:48 +0200

    Ajout d'un bouton de déconnexion

--- src/app/_components/header/header.component.ts
+++ src/app/_components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/_services/authentication/auth.service';
 import { HeaderTitleService } from 'src/app/_services/title/header-title.service';
 
 @Component({
@@ -11,12 +12,16 @@ export class HeaderComponent {
   public menu_items = [{ menuName: 'Actions', url: '/login', color: 'danger' }, { menuName: 'Users', url: '/home', color: 'secondary' }, { menuName: 'Paramètres', url: '/project', color: 'primary' }];
   private headerTitleService = inject(HeaderTitleService);
   public pageTitle$: Observable<string> = this.headerTitleService.title;
-
+  private authService = inject(AuthService);
   constructor() { }
 
 
   ionViewWillEnter() {
     console.log('test view');
+  }
+
+  logoutClick() {
+    this.authService.logout();
   }
 
 }
