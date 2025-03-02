c07fd28b294d78aa5e932e7c464812150306b5fb
Joris-G <66126635+Joris-G@users.noreply.github.com>
2025-03-03 00:58:11 +0100

    chrono page update

--- src/app/_components/_chrono/create-chrono-form/create-chrono-form.component.ts
+++ src/app/_components/_chrono/create-chrono-form/create-chrono-form.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule, IonInput } from '@ionic/angular';
+import { ChronoService } from 'src/app/_services/chrono/chrono.service';
 
 @Component({
   selector: 'app-create-chrono-form',
@@ -10,5 +11,10 @@ import { IonicModule, IonInput } from '@ionic/angular';
   imports:[FormsModule, IonicModule]
 })
 export class CreateChronoFormComponent   {
+  private readonly chronoService = inject(ChronoService);
+  chronoName: string = "";
+  newChronoInput() {
+    this.chronoService.createChrono({ name: this.chronoName });
+  }
 
 }
