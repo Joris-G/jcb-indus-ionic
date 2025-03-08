397e63cf6d0a70184ae35e40cc2776103a91fcb2
Joris-G <66126635+Joris-G@users.noreply.github.com>
2025-03-08 23:45:57 +0100

    ajouter plusieurs pièces pour la création

--- src/app/_components/_chrono/create-chrono-form/create-chrono-form.component.ts
+++ src/app/_components/_chrono/create-chrono-form/create-chrono-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { ChronoService } from 'src/app/_services/chrono/chrono.service';
 
@@ -8,32 +8,80 @@ import { ChronoService } from 'src/app/_services/chrono/chrono.service';
   templateUrl: './create-chrono-form.component.html',
   styleUrls: ['./create-chrono-form.component.scss'],
   standalone: true,
-  imports: [FormsModule, IonicModule, ReactiveFormsModule]
+  imports: [IonicModule, ReactiveFormsModule]
 })
 export class CreateChronoFormComponent   {
-  private readonly fb = inject(FormBuilder);
-  createChronoForm!: FormGroup;
-  private readonly chronoService = inject(ChronoService);
-
-  newChronoInput() {
-    this.chronoService.createChrono(this.createChronoForm.value)
-      .subscribe(
-        {
-          next: (value) => {
-            this.createChronoForm.reset()
-          },
-          error(err) {
-
-          },
-        }
-      );
+  getGroup(_t54: AbstractControl<any, any>): FormGroup<any> {
+    return _t54 as FormGroup
   }
+  test(_t54: AbstractControl<any, any>) {
+    console.log(_t54);
+  }
+
+  getControl(formGroup: AbstractControl<any, any>, controlName: string): FormControl {
+    return formGroup.get(controlName) as FormControl;
+  }
+
+
+  private readonly fb = inject(FormBuilder);
+  readonly chronoService = inject(ChronoService);
+  createChronoForm!: FormGroup;
+
   constructor() {
     this.createChronoForm = this.fb.group({
       name: ['', Validators.required],
       project: ['', Validators.nullValidator],
-      part: ['', Validators.nullValidator],
-      designation: ['', Validators.nullValidator]
-    })
+      goal: ['', Validators.nullValidator],
+      chronos: this.fb.array([])
+    });
+  }
+
+  get chronos(): FormArray {
+    return this.createChronoForm.get('chronos') as FormArray;
+  }
+
+  getParts(chronoIndex: number): FormArray {
+    const chrono = this.chronos.at(chronoIndex);
+    return chrono.get('parts') as FormArray;
+  }
+
+  addAChronoClick() {
+    const newChrono = this.fb.group({
+      parts: this.fb.array([])
+    });
+    this.chronos.push(newChrono);
+  }
+
+  addAPartClick(chronoIndex: number) {
+    const chrono = this.chronos.at(chronoIndex);
+    if (chrono) {
+      const parts = chrono.get('parts') as FormArray;
+      const newPart = this.fb.group({
+        partNumber: [''],
+        designation: [''],
+        workorder: ['']
+      });
+      parts.push(newPart);
+    }
+  }
+
+  newChronoInput() {
+    console.log(this.createChronoForm.value);
+    this.chronoService.createChrono(this.createChronoForm.value)
+      .subscribe({
+        next: (value) => {
+          this.createChronoForm.reset();
+          this.clearFormArray(this.chronos);
+        },
+        error: (err) => {
+          console.error(err);
+        }
+      });
+  }
+
+  clearFormArray(formArray: FormArray) {
+    while (formArray.length !== 0) {
+      formArray.removeAt(0);
+    }
   }
 }
