8011dacd7b7e0aed76ad2fb593289a1169457ce3
Joris-G <66126635+Joris-G@users.noreply.github.com>
2025-03-08 15:15:45 +0100

    chrono object update

--- src/app/_interfaces/chrono.interface.ts
+++ src/app/_interfaces/chrono.interface.ts
@@ -1,7 +1,9 @@
 export interface Chrono{
     id:number
     name:string
-
+    project: string
+    designation: string
+    partNumber: string
 }
 
 export type ChronoCreation = Omit<Chrono,"id">;
