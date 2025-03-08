4cae536d4b7ae3e8c8cb5eaca09b919e35fd40eb
Joris-G <66126635+Joris-G@users.noreply.github.com>
2025-03-08 15:15:30 +0100

    create new chrono update

--- src/app/_components/_chrono/create-chrono-form/create-chrono-form.component.ts
+++ src/app/_components/_chrono/create-chrono-form/create-chrono-form.component.ts
@@ -1,6 +1,6 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { IonicModule, IonInput } from '@ionic/angular';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 import { ChronoService } from 'src/app/_services/chrono/chrono.service';
 
 @Component({
@@ -8,13 +8,32 @@ import { ChronoService } from 'src/app/_services/chrono/chrono.service';
   templateUrl: './create-chrono-form.component.html',
   styleUrls: ['./create-chrono-form.component.scss'],
   standalone: true,
-  imports:[FormsModule, IonicModule]
+  imports: [FormsModule, IonicModule, ReactiveFormsModule]
 })
 export class CreateChronoFormComponent   {
+  private readonly fb = inject(FormBuilder);
+  createChronoForm!: FormGroup;
   private readonly chronoService = inject(ChronoService);
-  chronoName: string = "";
-  newChronoInput() {
-    this.chronoService.createChrono({ name: this.chronoName });
-  }
 
+  newChronoInput() {
+    this.chronoService.createChrono(this.createChronoForm.value)
+      .subscribe(
+        {
+          next: (value) => {
+            this.createChronoForm.reset()
+          },
+          error(err) {
+
+          },
+        }
+      );
+  }
+  constructor() {
+    this.createChronoForm = this.fb.group({
+      name: ['', Validators.required],
+      project: ['', Validators.nullValidator],
+      part: ['', Validators.nullValidator],
+      designation: ['', Validators.nullValidator]
+    })
+  }
 }
