d95742cb155aa46db911705a581d2fcbca37877d
Joris-G <66126635+Joris-G@users.noreply.github.com>
2025-03-08 15:13:36 +0100

    chrono list init

--- src/app/_components/_chrono/chrono-list/chrono-list.component.ts
+++ src/app/_components/_chrono/chrono-list/chrono-list.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { IonicModule, IonList } from '@ionic/angular';
+import { ChronoService } from 'src/app/_services/chrono/chrono.service';
 
 @Component({
   selector: 'app-chrono-list',
   templateUrl: './chrono-list.component.html',
   styleUrls: ['./chrono-list.component.scss'],
   standalone:true,
-  imports:[]
+  imports: [IonicModule, RouterModule]
 })
-export class ChronoListComponent  implements OnInit {
+export class ChronoListComponent {
+  readonly chronoService = inject(ChronoService);
 
-  constructor() { }
-
-  ngOnInit() {}
 
 }
