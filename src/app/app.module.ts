0ada390ac75893e68bfa1127cd6c09ba4c222591
Joris-G <66126635+Joris-G@users.noreply.github.com>
2025-03-30 21:23:29 +0200

    angular core update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { provideRouter, RouteReuseStrategy, withComponentInputBinding } from '@angular/router';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DelayInterceptor } from './_httpInterceptors/delay.interceptor';
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(),
-    AppRoutingModule,
-    HttpClientModule,
-  ],
-  providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, {
-    provide: HTTP_INTERCEPTORS,
-    useClass: DelayInterceptor,
-    multi: true
-    },
-  ],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        IonicModule.forRoot(),
+        AppRoutingModule], providers: [
+        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, {
+            provide: HTTP_INTERCEPTORS,
+            useClass: DelayInterceptor,
+            multi: true
+        },
+        provideHttpClient(withInterceptorsFromDi()),
+    ] })
 export class AppModule { }
